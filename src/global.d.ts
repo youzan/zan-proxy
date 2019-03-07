0a39048b346e8a6a148809fb48fefa2e84c489cf
kainstar <461345042@qq.com>
2019-03-07 18:00:29 +0800

    fix: 修复测试流程

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,7 @@ type MayExistProp<Obj, Key> = Key extends keyof Obj ? Obj[Key] : void;
 declare namespace NodeJS {
   declare interface Global {
     __root: string;
+    __site: string;
     __static: string;
     __resource: string;
   }
